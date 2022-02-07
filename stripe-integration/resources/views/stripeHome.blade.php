d6be078ced3288ca2e68d22fcbe87ce0a6b7924a
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-02-07 23:48:14 +0500

    Installed laravel/Breeze Authentication Scafolding

---
+++ stripe-integration/resources/views/stripeHome.blade.php
@@ -0,0 +1,16 @@
+<!-- @extends('app') -->
+
+<!-- @push('css') -->
+
+<!-- @endpush -->
+
+<!-- @section('content') -->
+
+<h1>home</h1>
+
+<!-- @endsection -->
+
+
+<!-- @push('js') -->
+
+<!-- @endpush -->
